bith3ad/optee_os

commit 21a4ce17fb4d13c9e09d3858dda90143bcf41dd3

plat-vexpress: add QEMU sbsa-ref platform
Add support for the QEMU sbsa-ref platform. This platform is similar to
the QEMU virt platform, but with different memory configuration and
device addresses.

Signed-off-by: Jens Wiklander <jens.wiklander@linaro.org>
Reviewed-by: Etienne Carriere <etienne.carriere@foss.st.com>

core/arch/arm/plat-vexpress/conf.mk
@@ -25,6 +25,10 @@ include core/arch/arm/cpu/cortex-armv8-0.mk
 CFG_ARM64_core ?= y
 supported-ta-targets ?= ta_arm64 ta_arm32
 endif
+ifeq ($(PLATFORM_FLAVOR),qemu_sbsa)
+include core/arch/arm/cpu/cortex-armv8-0.mk
+CFG_ARM64_core ?= y
+endif
 
 
 ifeq ($(platform-debugger-arm),1)
@@ -169,6 +173,21 @@ else ifneq ($(CFG_CORE_SEL2_SPMC),y)
 CFG_CORE_ASYNC_NOTIF_GIC_INTID ?= 219
 endif
 endif #PLATFORM_FLAVOR==qemu_armv8a
+
+ifeq ($(PLATFORM_FLAVOR),qemu_sbsa)
+CFG_CORE_HEAP_SIZE ?= 196608
+CFG_HALT_CORES_ON_PANIC ?= y
+CFG_TEE_CORE_NB_CORE ?= 4
+CFG_AUTO_MAX_PA_BITS ?= y
+CFG_CORE_RESERVED_SHM ?= n
+ifeq ($(CFG_CORE_SEL2_SPMC),y)
+else
+$(call force,CFG_ARM_GICV3,y)
+$(call force,CFG_DT_ADDR,0)
+$(call force,CFG_DT,y)
+CFG_DTB_MAX_SIZE ?= 0x100000
+endif
+endif #PLATFORM==qemu_sbsa
 
 ifneq (,$(filter $(PLATFORM_FLAVOR),qemu_virt qemu_armv8a))
 CFG_DT_DRIVER_EMBEDDED_TEST ?= y

core/arch/arm/plat-vexpress/platform_config.h
@@ -81,6 +81,13 @@
 #define CONSOLE_UART_BASE	UART1_BASE
 #define IT_CONSOLE_UART		IT_UART1
 
+#elif defined(PLATFORM_FLAVOR_qemu_sbsa)
+#define GIC_BASE		0x40060000
+#define SECURE_UART_BASE	0x60030000
+#define IT_SECURE_UART		40
+
+#define CONSOLE_UART_BASE	SECURE_UART_BASE
+#define IT_CONSOLE_UART		IT_SECURE_UART
 #else
 #error "Unknown platform flavor"
 #endif
@@ -140,6 +147,11 @@
 #define GIC_REDIST_SIZE		0x00F60000
 #endif
 
+#elif defined(PLATFORM_FLAVOR_qemu_sbsa)
+#define GICD_OFFSET		0
+#define GICC_OFFSET		0x10000
+#define GIC_REDIST_BASE		0x40080000
+#define GIC_REDIST_SIZE		0x04000000
 #else
 #error "Unknown platform flavor"
 #endif

commit 6c69010c5c1b8ca8de7c7b7b9dfc2637cf000287

plat-vexpress: conf: fix HALT_CORES_ON_PANIC config
CFG_HALT_CORES_ON_PANIC is disabled since it depends on
CFG_MULTI_CORE_HALTING:
  mk/config.mk:1241: Warning: Disabling CFG_HALT_CORES_ON_PANIC [requires CFG_MULTI_CORE_HALTING]

All other vexpress machines already use CFG_MULTI_CORE_HALTING.

Fixes: 21a4ce17f ("plat-vexpress: add QEMU sbsa-ref platform")
Signed-off-by: Marco Felsch <m.felsch@pengutronix.de>

core/arch/arm/plat-vexpress/conf.mk
@@ -176,7 +176,7 @@ endif #PLATFORM_FLAVOR==qemu_armv8a
 
 ifeq ($(PLATFORM_FLAVOR),qemu_sbsa)
 CFG_CORE_HEAP_SIZE ?= 196608
-CFG_HALT_CORES_ON_PANIC ?= y
+CFG_MULTI_CORE_HALTING ?= y
 CFG_TEE_CORE_NB_CORE ?= 4
 CFG_AUTO_MAX_PA_BITS ?= y
 CFG_CORE_RESERVED_SHM ?= n